47d6dfeab7c00fb41a20649d177e7c7a1ec51dd1
Alex Franco <alex.franco9@icloud.com>
2021-07-12 17:58:25 -0500

    initial commit

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -3,50 +3,94 @@ import React, { useState } from 'react';
 
 
 const UserForm = props => {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setconfirmPassword] = useState("");
+    const [Firstname, setFirstname] = useState("");
+    const [Lastname, setLastname] = useState("");
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+    const [ConfirmPassword, setConfirmPassword] = useState("");
 
     const [formSubmitted, setFormSubmitted] = useState(false);
 
     const createUser = (e) => {
         e.preventDefault();
-        const newUser = { username, email, password, confirmPassword };
+        const newUser = { Firstname, Lastname, Email, Password, ConfirmPassword };
         console.log("Welcome!", newUser);
         setFormSubmitted(true);
+    };
+
+    const TextValid = input => {
+        if(input.length < 2) {
+            return false;
+        }
+        return true;
+    };
+
+    const EmailValid = input => {
+        if(input.length < 5) {
+            return false;
+        }
+        return true;
+    };
+
+    const PasswordValid = input => {
+        if(input.length < 8) {
+            return false;
+        }
+        return true;
+    };
+
+    const ConfirmValid = input => {
+        if(input.value !== Password) {
+            return false;
+        }
+        return true;
     };
 
     return (
         <div className="container">
             <form onSubmit={createUser}>
                 <div>
-                    <label htmlFor="username">Username: </label>
-                    <input className="form-control" type="text" onChange={(e) => setUsername(e.target.value)} />
+                    <label htmlFor="Firstname">Firstname: </label>
+                    <input className="form-control" type="text" name="Firstname" value={Firstname} onChange={(e) => setFirstname(e.target.value)} />
+                    {TextValid(Firstname) ? "" : <p>Field must be at least 2 characters long.</p> }
                 </div>
                 <div>
-                    <label htmlFor="email">Email Address: </label>
-                    <input className="form-control" type="text" onChange={(e) => setEmail(e.target.value)} />
+                    <label htmlFor="Lastname">Lastname: </label>
+                    <input className="form-control" type="text" name="Lastname" value={Lastname} onChange={(e) => setLastname(e.target.value)} />
+                    {TextValid(Lastname) ? "" : <p>Field must be at least 2 characters long.</p> }
                 </div>
                 <div>
-                    <label htmlFor="password">Password: </label>
-                    <input className="form-control" type="password" onChange={(e) => setPassword(e.target.value)} />
+                    <label htmlFor="Email">Email Address: </label>
+                    <input className="form-control" type="text" name="Email" value={Email} onChange={(e) => setEmail(e.target.value)} />
+                    {EmailValid(Email) ? "" : <p>Field must be at least 8 characters long.</p> }
                 </div>
                 <div>
-                    <label htmlFor="confirmPassword">Confirm Password: </label>
-                    <input className="form-control" type="password" onChange={(e) => setconfirmPassword(e.target.value)} />
+                    <label htmlFor="Password">Password: </label>
+                    <input className="form-control" type="password" name="Password" value={Password} onChange={(e) => setPassword(e.target.value)} />
+                    {PasswordValid(Password) ? "" : <p>Field must be at least 8 characters long.</p> }
+                </div>
+                <div>
+                    <label htmlFor="ConfirmPassword">Confirm Password: </label>
+                    <input className="form-control" type="password" name="ConfirmPassword" value={ConfirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                    {ConfirmValid(ConfirmPassword) ? "" : <p>Passwords must match!</p> }
                 </div>
                 <br/>
                 <div>
-                    <input type="submit" value="Create User!" className="btn btn-outline-secondary"/>
+                    <input disabled={!TextValid(Firstname, Lastname) || !EmailValid(Email) || !PasswordValid(Password) || !ConfirmValid(ConfirmPassword)} type="submit" value="Create User!" className="btn btn-outline-secondary"/>
                 </div>
             </form>
             <br/>
+            {
+                formSubmitted ?
+                <>
                 <h3> |Your Form Data| </h3>
-                <h5>Username: {username}</h5>
-                <h5>Email: {email}</h5>
-                <h5>Password: {password}</h5>
-                <h5>Confirm Password: {confirmPassword}</h5>
+                <h5>Firstname: {Firstname}</h5>
+                <h5>Lastname: {Lastname}</h5>
+                <h5>Email: {Email}</h5>
+                <h5>Password: {Password}</h5>
+                <h5>Confirm Password: {ConfirmPassword}</h5>
+                </>: ""
+            }
         </div>
     );
 };
